9e7b692e078eb0531a8b6a873b33d017813bb0f6
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-01 15:12:53 +0200

    Use all forms of data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -25,7 +25,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Sever was created!';
+    this.serverCreationStatus = 'Sever was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
